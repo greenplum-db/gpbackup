ca65610be84ef0d6e590406596a6bdf357f0e50e
Chris Hajas <chajas@pivotal.io>
2018-01-30 14:11:44 -0800

    Add support for backing up base type storage options
    Author: Chris Hajas <chajas@pivotal.io>

--- backup/queries_relations.go
+++ backup/queries_relations.go
@@ -354,7 +354,7 @@ func GetPartitionTemplates(connection *utils.DBConn) map[uint32]string {
 }
 
 func GetStorageOptions(connection *utils.DBConn) map[uint32]string {
-	query := ` SELECT oid, array_to_string(reloptions, ', ') AS value FROM pg_class WHERE reloptions IS NOT NULL;`
+	query := `SELECT oid, array_to_string(reloptions, ', ') AS value FROM pg_class WHERE reloptions IS NOT NULL;`
 	return SelectAsOidToStringMap(connection, query)
 }
 
