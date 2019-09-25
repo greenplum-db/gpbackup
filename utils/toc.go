e3002d57cd54c9fef5160543553becc32f1f2e48
Lav Jain <ljain@pivotal.io>
2019-09-24 17:40:49 -0700

    Fix formatting issues in toc file

--- utils/toc.go
+++ utils/toc.go
@@ -79,7 +79,7 @@ func (toc *TOC) WriteToFileAndMakeReadOnly(filename string) {
 	tocContents, err := yaml.Marshal(toc)
 	gplog.FatalOnError(err)
 
-	tocFile, err := os.OpenFile(filename, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0644)
+	tocFile, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
 	gplog.FatalOnError(err)
 
 	_, err = tocFile.Write(tocContents)
@@ -102,7 +102,7 @@ func (toc *SegmentTOC) WriteToFileAndMakeReadOnly(filename string) error {
 		return err
 	}
 
-	tocFile, err := os.OpenFile(filename, os.O_CREATE | os.O_EXCL | os.O_WRONLY, 0644)
+	tocFile, err := os.OpenFile(filename, os.O_CREATE|os.O_EXCL|os.O_WRONLY, 0644)
 	if err != nil {
 		return err
 	}
