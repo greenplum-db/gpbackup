greenplum-db/gpbackup

commit a6c2839f2aac1fd47955bdee692c67e71b813550

Enforce incremental restore to require --data-only
Incremental restore only restores data incrementally and doesn't restore
any metadata (predata/postdata). Validation check has been added to
enforce restore with --incremental to only work with --data-only

restore/validate.go
@@ -271,4 +271,7 @@ func ValidateFlagCombinations(flags *pflag.FlagSet) {
 		!flags.Changed(options.DATA_ONLY) {
 		gplog.Fatal(errors.Errorf("Cannot use --truncate-table without --include-table or --include-table-file and without --data-only"), "")
 	}
+	if flags.Changed(options.INCREMENTAL) && !flags.Changed(options.DATA_ONLY) {
+		gplog.Fatal(errors.Errorf("Cannot use --incremental without --data-only"), "")
+	}
 }

end_to_end/incremental_test.go
@@ -433,7 +433,7 @@ EOF1`, backupDir)
 
 				// Restore the incremental backup. We should see gprestore
 				// not error out due to already existing tables.
-				gprestore(gprestorePath, restoreHelperPath, incrementalBackupTimestamp, "--redirect-db", "restoredb", "--incremental")
+				gprestore(gprestorePath, restoreHelperPath, incrementalBackupTimestamp, "--redirect-db", "restoredb", "--incremental", "--data-only")
 
 				// Cleanup
 				testhelper.AssertQueryRuns(backupConn,
@@ -451,13 +451,34 @@ EOF1`, backupDir)
 					"INSERT INTO zoo VALUES (1);")
 				incrementalBackupTimestamp := gpbackup(gpbackupPath, backupHelperPath, "--leaf-partition-data", "--incremental")
 				gprestore(gprestorePath, restoreHelperPath, backupTimestamp, "--redirect-db", "restoredb")
-				gprestore(gprestorePath, restoreHelperPath, incrementalBackupTimestamp, "--redirect-db", "restoredb", "--incremental")
+				gprestore(gprestorePath, restoreHelperPath, incrementalBackupTimestamp, "--redirect-db", "restoredb", "--incremental", "--data-only")
 
 				// Cleanup
 				testhelper.AssertQueryRuns(backupConn,
 					"DROP TABLE IF EXISTS zoo;")
 				testhelper.AssertQueryRuns(restoreConn,
 					"DROP TABLE IF EXISTS zoo;")
+			})
+			It("Does not incremental restore without --data-only", func() {
+				args := []string{
+					"--timestamp", "23432432",
+					"--incremental",
+					"--redirect-db", "restoredb"}
+				cmd := exec.Command(gprestorePath, args...)
+				output, err := cmd.CombinedOutput()
+				Expect(err).To(HaveOccurred())
+				Expect(string(output)).To(ContainSubstring("Cannot use --incremental without --data-only"))
+			})
+			It("Does not incremental restore with --metadata-only", func() {
+				args := []string{
+					"--timestamp", "23432432",
+					"--incremental", "--metadata-only",
+					"--redirect-db", "restoredb"}
+				cmd := exec.Command(gprestorePath, args...)
+				output, err := cmd.CombinedOutput()
+				Expect(err).To(HaveOccurred())
+				Expect(string(output)).To(ContainSubstring(
+					"The following flags may not be specified together: truncate-table, metadata-only, incremental, redirect-schema"))
 			})
 		})
 		Context("No DDL no partitioning", func() {
@@ -528,7 +549,7 @@ EOF1`, backupDir)
 				})
 				It("Restores only tables included by use if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--include-table", "old_schema.old_table1",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table1"] = 20
@@ -540,7 +561,7 @@ EOF1`, backupDir)
 				})
 				It("Does not restore tables excluded by user if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--exclude-table", "new_schema.new_table1",
 						"--exclude-table", "new_schema.new_table2",
 						"--redirect-db", "restoredb")
@@ -553,7 +574,7 @@ EOF1`, backupDir)
 				})
 				It("Restores only schemas included by user if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--include-schema", "old_schema",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table1"] = 20
@@ -564,7 +585,7 @@ EOF1`, backupDir)
 				})
 				It("Does not restore schemas excluded by user if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--exclude-schema", "new_schema",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table1"] = 20
@@ -616,7 +637,7 @@ EOF1`, backupDir)
 				It("Does not restore old/new tables and exits gracefully", func() {
 					args := []string{
 						"--timestamp", timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--redirect-db", "restoredb"}
 					cmd := exec.Command(gprestorePath, args...)
 					output, err := cmd.CombinedOutput()
@@ -631,7 +652,7 @@ EOF1`, backupDir)
 				It("Only restores existing tables if --on-error-continue is specified", func() {
 					args := []string{
 						"--timestamp", timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--on-error-continue",
 						"--redirect-db", "restoredb"}
 					cmd := exec.Command(gprestorePath, args...)
@@ -668,7 +689,7 @@ EOF1`, backupDir)
 				})
 				It("Updates data in existing tables", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table1"] = 20
 					oldSchemaTupleCounts["old_schema.old_table2"] = 30
@@ -679,7 +700,7 @@ EOF1`, backupDir)
 				})
 				It("Updates only tables included by user if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--include-table", "old_schema.old_table1",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table1"] = 20
@@ -690,7 +711,7 @@ EOF1`, backupDir)
 				})
 				It("Does not update tables excluded by user if user input is provided", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--exclude-table", "old_schema.old_table1",
 						"--redirect-db", "restoredb")
 					oldSchemaTupleCounts["old_schema.old_table2"] = 30
@@ -701,7 +722,7 @@ EOF1`, backupDir)
 				})
 				It("Does not update anything if user excluded all tables", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--exclude-table", "old_schema.old_table1",
 						"--exclude-table", "old_schema.old_table2",
 						"--redirect-db", "restoredb")
@@ -712,7 +733,7 @@ EOF1`, backupDir)
 				})
 				It("Does not update tables if user input is provided and schema is not included", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--include-schema", "public",
 						"--redirect-db", "restoredb")
 					assertSchemasExist(restoreConn, 4)
@@ -722,7 +743,7 @@ EOF1`, backupDir)
 				})
 				It("Does not restore tables if user input is provide and schema is excluded by user", func() {
 					gprestore(gprestorePath, restoreHelperPath, timestamp,
-						"--incremental",
+						"--incremental", "--data-only",
 						"--exclude-schema", "old_schema",
 						"--redirect-db", "restoredb")
 					assertSchemasExist(restoreConn, 4)

restore/restore.go
@@ -125,8 +125,13 @@ func DoRestore() {
 	metadataFilename := globalFPInfo.GetMetadataFilePath()
 	isDataOnly := backupConfig.DataOnly || MustGetFlagBool(options.DATA_ONLY)
 	isMetadataOnly := backupConfig.MetadataOnly || MustGetFlagBool(options.METADATA_ONLY)
+	isIncremental := MustGetFlagBool(options.INCREMENTAL)
 
-	if !isDataOnly {
+	if isIncremental {
+		verifyIncrementalState()
+	}
+
+	if !isDataOnly && !isIncremental {
 		restorePredata(metadataFilename)
 	}
 
@@ -141,7 +146,7 @@ func DoRestore() {
 		restoreData()
 	}
 
-	if !isDataOnly && !MustGetFlagBool(options.INCREMENTAL) {
+	if !isDataOnly && !isIncremental {
 		restorePostdata(metadataFilename)
 	}
 
@@ -180,87 +185,86 @@ func restoreGlobal(metadataFilename string) {
 	gplog.Info("Global database metadata restore complete")
 }
 
+func verifyIncrementalState() {
+	lastRestorePlanEntry := backupConfig.RestorePlan[len(backupConfig.RestorePlan)-1]
+	tableFQNsToRestore := lastRestorePlanEntry.TableFQNs
+
+	existingSchemas, err := GetExistingSchemas()
+	gplog.FatalOnError(err)
+	existingTableFQNs, err := GetExistingTableFQNs()
+	gplog.FatalOnError(err)
+
+	existingSchemasMap := make(map[string]Empty)
+	for _, schema := range existingSchemas {
+		existingSchemasMap[schema] = Empty{}
+	}
+	existingTablesMap := make(map[string]Empty)
+	for _, table := range existingTableFQNs {
+		existingTablesMap[table] = Empty{}
+	}
+
+	var schemasToCreate []string
+	var tableFQNsToCreate []string
+	var schemasExcludedByUserInput []string
+	var tablesExcludedByUserInput []string
+	for _, table := range tableFQNsToRestore {
+		schemaName := strings.Split(table, ".")[0]
+		if utils.SchemaIsExcludedByUser(opts.IncludedSchemas, opts.ExcludedSchemas, schemaName) {
+			if !utils.Exists(schemasExcludedByUserInput, schemaName) {
+				schemasExcludedByUserInput = append(schemasExcludedByUserInput, schemaName)
+			}
+			tablesExcludedByUserInput = append(tablesExcludedByUserInput, table)
+			continue
+		}
+
+		if _, exists := existingTablesMap[table]; !exists {
+			if utils.RelationIsExcludedByUser(opts.IncludedRelations, opts.ExcludedRelations, table) {
+				tablesExcludedByUserInput = append(tablesExcludedByUserInput, table)
+			} else {
+				_, schemaExists := existingSchemasMap[schemaName]
+				preFilteredToCreate := utils.Exists(schemasToCreate, schemaName)
+				if !schemaExists && !preFilteredToCreate {
+					schemasToCreate = append(schemasToCreate, schemaName)
+				}
+				tableFQNsToCreate = append(tableFQNsToCreate, table)
+			}
+		}
+	}
+
+	var missing []string
+	if len(schemasToCreate) > 0 && !MustGetFlagBool(options.ON_ERROR_CONTINUE) {
+		missing = schemasToCreate
+	}
+	if len(tableFQNsToCreate) > 0 && !MustGetFlagBool(options.ON_ERROR_CONTINUE) {
+		missing = append(missing, tableFQNsToCreate...)
+	}
+	if missing != nil {
+		err = errors.Errorf("Following objects are missing from the target database: %v", missing)
+		gplog.FatalOnError(err)
+	}
+}
+
 func restorePredata(metadataFilename string) {
 	if wasTerminated {
 		return
 	}
 	gplog.Info("Restoring pre-data metadata")
-
-	if MustGetFlagBool(options.INCREMENTAL) {
-		lastRestorePlanEntry := backupConfig.RestorePlan[len(backupConfig.RestorePlan)-1]
-		tableFQNsToRestore := lastRestorePlanEntry.TableFQNs
-
-		existingSchemas, err := GetExistingSchemas()
-		gplog.FatalOnError(err)
-		existingTableFQNs, err := GetExistingTableFQNs()
-		gplog.FatalOnError(err)
-
-		existingSchemasMap := make(map[string]Empty)
-		for _, schema := range existingSchemas {
-			existingSchemasMap[schema] = Empty{}
-		}
-		existingTablesMap := make(map[string]Empty)
-		for _, table := range existingTableFQNs {
-			existingTablesMap[table] = Empty{}
-		}
-
-		var schemasToCreate []string
-		var tableFQNsToCreate []string
-		var schemasExcludedByUserInput []string
-		var tablesExcludedByUserInput []string
-		for _, table := range tableFQNsToRestore {
-			schemaName := strings.Split(table, ".")[0]
-			if utils.SchemaIsExcludedByUser(opts.IncludedSchemas, opts.ExcludedSchemas, schemaName) {
-				if !utils.Exists(schemasExcludedByUserInput, schemaName) {
-					schemasExcludedByUserInput = append(schemasExcludedByUserInput, schemaName)
-				}
-				tablesExcludedByUserInput = append(tablesExcludedByUserInput, table)
-				continue
-			}
-
-			if _, exists := existingTablesMap[table]; !exists {
-				if utils.RelationIsExcludedByUser(opts.IncludedRelations, opts.ExcludedRelations, table) {
-					tablesExcludedByUserInput = append(tablesExcludedByUserInput, table)
-				} else {
-					_, schemaExists := existingSchemasMap[schemaName]
-					preFilteredToCreate := utils.Exists(schemasToCreate, schemaName)
-					if !schemaExists && !preFilteredToCreate {
-						schemasToCreate = append(schemasToCreate, schemaName)
-					}
-					tableFQNsToCreate = append(tableFQNsToCreate, table)
-				}
-			}
-		}
-
-		var missing []string
-		if len(schemasToCreate) > 0 && !MustGetFlagBool(options.ON_ERROR_CONTINUE) {
-			missing = schemasToCreate
-		}
-		if len(tableFQNsToCreate) > 0 && !MustGetFlagBool(options.ON_ERROR_CONTINUE) {
-			missing = append(missing, tableFQNsToCreate...)
-		}
-		if missing != nil {
-			err = errors.Errorf("Following objects are missing from the target database: %v", missing)
-			gplog.FatalOnError(err)
-		}
-	} else {
-		// if not incremental restore - assume database is empty and just filter based on user input
-		filters := NewFilters(opts.IncludedSchemas, opts.ExcludedSchemas, opts.IncludedRelations, opts.ExcludedRelations)
-		var schemaStatements []toc.StatementWithType
-		if opts.RedirectSchema == "" {
-			schemaStatements = GetRestoreMetadataStatementsFiltered("predata", metadataFilename, []string{"SCHEMA"}, []string{}, filters)
-		}
-		statements := GetRestoreMetadataStatementsFiltered("predata", metadataFilename, []string{}, []string{"SCHEMA"}, filters)
-
-		editStatementsRedirectSchema(statements, opts.RedirectSchema)
-		progressBar := utils.NewProgressBar(len(schemaStatements)+len(statements), "Pre-data objects restored: ", utils.PB_VERBOSE)
-		progressBar.Start()
-
-		RestoreSchemas(schemaStatements, progressBar)
-		ExecuteRestoreMetadataStatements(statements, "Pre-data objects", progressBar, utils.PB_VERBOSE, false)
-
-		progressBar.Finish()
+	// if not incremental restore - assume database is empty and just filter based on user input
+	filters := NewFilters(opts.IncludedSchemas, opts.ExcludedSchemas, opts.IncludedRelations, opts.ExcludedRelations)
+	var schemaStatements []toc.StatementWithType
+	if opts.RedirectSchema == "" {
+		schemaStatements = GetRestoreMetadataStatementsFiltered("predata", metadataFilename, []string{"SCHEMA"}, []string{}, filters)
 	}
+	statements := GetRestoreMetadataStatementsFiltered("predata", metadataFilename, []string{}, []string{"SCHEMA"}, filters)
+
+	editStatementsRedirectSchema(statements, opts.RedirectSchema)
+	progressBar := utils.NewProgressBar(len(schemaStatements)+len(statements), "Pre-data objects restored: ", utils.PB_VERBOSE)
+	progressBar.Start()
+
+	RestoreSchemas(schemaStatements, progressBar)
+	ExecuteRestoreMetadataStatements(statements, "Pre-data objects", progressBar, utils.PB_VERBOSE, false)
+
+	progressBar.Finish()
 	if wasTerminated {
 		gplog.Info("Pre-data metadata restore incomplete")
 	} else {

commit edbf2cd147eaa67af42f063db04fd81af6093bd2

Restore flag validation fixes
- Support for include-schema and exclude-table. This fixes a regression caused by commit a6c2839

- Refactored redirect flag checks and separated truncate flag checks from it.

restore/validate.go
@@ -252,17 +252,25 @@ func ValidateFlagCombinations(flags *pflag.FlagSet) {
 	options.CheckExclusiveFlags(flags, options.DATA_ONLY, options.WITH_GLOBALS)
 	options.CheckExclusiveFlags(flags, options.DATA_ONLY, options.CREATE_DB)
 	options.CheckExclusiveFlags(flags, options.DEBUG, options.QUIET, options.VERBOSE)
-	options.CheckExclusiveFlags(flags,
-		options.EXCLUDE_SCHEMA, options.EXCLUDE_SCHEMA_FILE, options.EXCLUDE_RELATION, options.EXCLUDE_RELATION_FILE,
-		options.INCLUDE_SCHEMA, options.INCLUDE_SCHEMA_FILE, options.INCLUDE_RELATION, options.INCLUDE_RELATION_FILE)
+
+	options.CheckExclusiveFlags(flags, options.INCLUDE_SCHEMA, options.INCLUDE_RELATION, options.INCLUDE_RELATION_FILE)
+	options.CheckExclusiveFlags(flags, options.EXCLUDE_SCHEMA, options.INCLUDE_SCHEMA)
+	options.CheckExclusiveFlags(flags, options.EXCLUDE_SCHEMA, options.EXCLUDE_RELATION, options.INCLUDE_RELATION, options.EXCLUDE_RELATION_FILE, options.INCLUDE_RELATION_FILE)
+
 	options.CheckExclusiveFlags(flags, options.METADATA_ONLY, options.DATA_ONLY)
 	options.CheckExclusiveFlags(flags, options.PLUGIN_CONFIG, options.BACKUP_DIR)
-	options.CheckExclusiveFlags(flags,
-		options.TRUNCATE_TABLE, options.REDIRECT_SCHEMA, options.EXCLUDE_SCHEMA, options.EXCLUDE_SCHEMA_FILE,
-		options.EXCLUDE_RELATION, options.EXCLUDE_RELATION_FILE, options.INCLUDE_SCHEMA, options.INCLUDE_SCHEMA_FILE)
-	if flags.Changed(options.REDIRECT_SCHEMA) &&
-		!(flags.Changed(options.INCLUDE_RELATION) || flags.Changed(options.INCLUDE_RELATION_FILE)) {
-		gplog.Fatal(errors.Errorf("Cannot use --redirect-schema without --include-table or --include-table-file"), "")
+
+	if flags.Changed(options.REDIRECT_SCHEMA) {
+		// Redirect schema not compatible with any exclude flags and include schema flags
+		if flags.Changed(options.EXCLUDE_SCHEMA) || flags.Changed(options.EXCLUDE_SCHEMA_FILE) ||
+			flags.Changed(options.EXCLUDE_RELATION) || flags.Changed(options.EXCLUDE_RELATION_FILE) ||
+			flags.Changed(options.INCLUDE_SCHEMA) || flags.Changed(options.INCLUDE_SCHEMA_FILE) {
+			gplog.Fatal(errors.Errorf("Cannot use --redirect-schema with exclude flags or include schema flags"), "")
+		}
+		// Redirect schema requires include relation
+		if !(flags.Changed(options.INCLUDE_RELATION) || flags.Changed(options.INCLUDE_RELATION_FILE)) {
+			gplog.Fatal(errors.Errorf("Cannot use --redirect-schema without --include-table or --include-table-file"), "")
+		}
 	}
 	options.CheckExclusiveFlags(flags,
 		options.TRUNCATE_TABLE, options.METADATA_ONLY, options.INCREMENTAL, options.REDIRECT_SCHEMA)

end_to_end/end_to_end_suite_test.go
@@ -1354,4 +1354,14 @@ var _ = Describe("backup and restore end to end tests", func() {
 
 		assertArtifactsCleaned(restoreConn, timestamp)
 	})
+	It("runs gprestore with --include-schema and --exclude-table flag", func() {
+		timestamp := gpbackup(gpbackupPath, backupHelperPath,
+			"--metadata-only")
+		gprestore(gprestorePath, restoreHelperPath, timestamp,
+			"--redirect-db", "restoredb",
+			"--include-schema", "schema2",
+			"--exclude-table", "schema2.returns",
+			"--metadata-only")
+		assertRelationsCreated(restoreConn, 4)
+	})
 })