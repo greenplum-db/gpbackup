7f90bf41106ca7e5f20f0c296eaaade95ac52a40
Vasiliy Ivanov <ivi@arenadata.io>
2022-03-05 10:46:22 +0000

    change golang version to 1.17.6 according to eb19837

--- arenadata/run_gpbackup_tests.bash
+++ arenadata/run_gpbackup_tests.bash
@@ -14,6 +14,6 @@ source /usr/local/greenplum-db-devel/greenplum_path.sh;
 source ~/gpdb_src/gpAux/gpdemo/gpdemo-env.sh;
 gpconfig -c shared_preload_libraries -v dummy_seclabel;
 gpstop -ar;
-wget https://golang.org/dl/go1.14.15.linux-amd64.tar.gz;
-tar -C ~/ -xzf go1.14.15.linux-amd64.tar.gz;
+wget https://golang.org/dl/go1.17.6.linux-amd64.tar.gz;
+tar -C ~/ -xzf go1.17.6.linux-amd64.tar.gz;
 PATH=$PATH:~/go/bin GOPATH=~/go make depend build install test end_to_end -C go/src/github.com/greenplum-db/gpbackup/"
